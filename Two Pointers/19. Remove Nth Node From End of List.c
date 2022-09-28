cb34b603569fdd8c1feb2b68460a1efe31832679
Kim Hyun Bin <77542415+vanilladucky@users.noreply.github.com>
2022-09-28 21:19:40 +0900

    Create 19. Remove Nth Node From End of List.c

---
+++ Two Pointers/19. Remove Nth Node From End of List.c
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
+    struct ListNode *dummy = (struct ListNode*)malloc(sizeof(struct ListNode)*100);
+    dummy->next = head;
+    struct ListNode *ptr1 = dummy;
+    struct ListNode *ptr2 = dummy;
+    int i = 0;
+    
+    for (i=0; i<n; i++){
+        ptr2 = ptr2->next;
+    }
+    
+    while (ptr2->next != NULL){
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
+    }
+    
+    ptr1->next = ptr1->next->next;
+    
+    return dummy->next;
+}
