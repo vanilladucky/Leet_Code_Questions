8edd21602d758f287c194c3e5e2e8c2a297c825b
Kim Hyun Bin <77542415+vanilladucky@users.noreply.github.com>
2022-10-02 16:45:02 +0900

    Update 1155. Number of Dice Rolls With Target Sum.py

--- Dynamic Programming/1155. Number of Dice Rolls With Target Sum.py
+++ Dynamic Programming/1155. Number of Dice Rolls With Target Sum.py
@@ -1,28 +1,25 @@
 class Solution:
-	def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-
-		self.memo = {}
-
-		def solve(target,dices):
-
-			if((target,dices) in self.memo):
-				return self.memo[(target,dices)]
-
-			if(target == 0 and dices == 0):
-				return 1
-
-			if(target == 0):
-				return 0
-
-			if(dices < 1):
-				return 0
-
-			ans = 0
-			for i in range(1,k+1):
-				if(target >= i): # For faster execution, not really necessary to the whole algorithm
-					ans += solve(target-i,dices-1)
-
-			self.memo[(target,dices)] = ans
-			return ans
-
-		return solve(target,n) % (7 + 10**9)
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        memo = {}
+        
+        def dp(itr, summ):
+            
+            if (itr, summ) in memo:
+                return memo[(itr,summ)]
+            
+            if summ == target and itr == n:
+                return 1 
+            
+            if (summ == target) or itr >= n:
+                return 0
+            
+            ans = 0
+            
+            for i in range(1,k+1):
+                if target >= i:
+                    ans += dp(itr+1, summ+i)
+                
+            memo[(itr,summ)] = ans
+            return ans
+                
+        return dp(0,0) % (7 + 10**9)
