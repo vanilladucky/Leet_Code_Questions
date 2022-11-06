83f7436b786fbcd5cadf670b56b6843b7f8a27db
Kim Hyun Bin <77542415+vanilladucky@users.noreply.github.com>
2022-11-06 15:31:15 +0900

    Create 899. Orderly Queue.py

---
+++ Math/899. Orderly Queue.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/orderly-queue/
+class Solution:
+    def orderlyQueue(self, s: str, k: int) -> str:
+        if k == 1:
+            return min(s[i:] + s[:i] for i in range(len(s)))
+        else:
+            return ''.join(sorted(s))
