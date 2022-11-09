8fff700455b96db6ba64bd8c972bcdf3f41959e5
Kim Hyun Bin <77542415+vanilladucky@users.noreply.github.com>
2022-11-09 13:41:42 +0900

    Create 125. Valid Palindrome.py

---
+++ Two Pointers/125. Valid Palindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(filter(str.isalnum, s)).lower()
+        left, right = 0, len(s)-1
+        while left <= right:
+            if s[left] == s[right]:
+                left+=1
+                right-=1
+            else:
+                return False
+        return True
