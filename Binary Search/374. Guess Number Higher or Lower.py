39318f3c16a0cdeed5da3c14d863834ba94620e3
Kim Hyun Bin <77542415+vanilladucky@users.noreply.github.com>
2022-11-16 19:09:51 +0900

    Create 374. Guess Number Higher or Lower.py

---
+++ Binary Search/374. Guess Number Higher or Lower.py
@@ -0,0 +1,18 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        left, right = 1, n
+        
+        while guess(tmp:=(left+right)//2) != 0:
+            if guess(tmp) == -1:
+                right = tmp-1
+            else:
+                left = tmp + 1
+            
+        return tmp
